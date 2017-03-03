d3a70530ce0fabd0c9105beeb0a984c7b823d99b
ali <ali@ali.ali>
2017-03-03 20:40:19 +0800

    gbonrails deleted

--- gbonrails/test/controllers/fake_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class FakeControllerTest < ActionDispatch::IntegrationTest
-  test "should get fakefile" do
-    get fake_fakefile_url
-    assert_response :success
-  end
-
-end
